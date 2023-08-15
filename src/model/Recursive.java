18dafd63668deb9f8ba760bbd2f50ad843c05d63
Juan Manuel Casanova <juancasanovamarin@gmail.com>
2023-08-15 15:37:03 -0500

    Revisen errores bv

--- src/model/Recursive.java
+++ src/model/Recursive.java
@@ -91,4 +91,17 @@ public class Recursive {
 
         return r;
     }
+
+    public boolean meAmasONo(int i) {
+
+        boolean love = false;
+
+        if (i == 0) {
+            lov //quedo a medias .o.
+        } if (i == 0) {
+
+        }
+
+        return false;
+    }
 }
